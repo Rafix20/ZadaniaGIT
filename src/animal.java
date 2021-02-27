2e97f4572032c5313bf42e5a0289e45457929948
Rafał <rafal.lenovo2003@gmail.com>
2021-02-27 20:37:13 +0100

    hamburger

--- src/animal.java
+++ src/animal.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class animal {
+public class animal {
+    public String name;
+
+
+    public animal(String name) {
+        this.name = name;
+    }
+
+
+    public void showName(){
+        System.out.println("Imie zwierzęcia: " +name);
+    }
 }
